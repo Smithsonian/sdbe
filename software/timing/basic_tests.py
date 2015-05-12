63e3d4a43d2da5da2e551f0fba7e16370479cb78
Katherine Rosenfeld <krosenf@gmail.com>
2015-05-12 07:14:41 -0700

    added cufft timing to basic_tests.py

---
+++ software/timing/basic_tests.py
@@ -0,0 +1,100 @@
+from timeit import timeit
+
+setup_cpu = """
+from scipy.signal import resample
+from pyfftw.interfaces import numpy_fft as fftw
+fftwrfft = fftw.rfft
+fftwirfft = fftw.irfft
+from numpy.fft import rfft, irfft
+import numpy as np
+n_swarm = 32768
+n_swarm39 = n_swarm * 39
+snapspersec = 2496e6 / n_swarm
+n_r2dbe39 = 2097152 / 2
+x_swarm = np.random.randn(n_swarm)
+x_swarm39 = np.random.randn(n_swarm39)
+f_swarm = rfft(x_swarm)
+t_swarm39 = np.arange(n_swarm39) / 2496.
+t_r2dbe39 = np.arange(n_r2dbe39) / 2048.
+inn = np.round(t_r2dbe39 * 2496.).astype(int)
+ia = np.floor(t_r2dbe39 * 2496.).astype(int)
+ib = np.ceil(t_r2dbe39 * 2496.).astype(int)
+ib[(ia-ib == 0)] += 1
+tt = t_swarm39[ib] - t_swarm39[ia]
+cb = (t_r2dbe39 - t_swarm39[ia])/tt
+ca = 1.-cb
+"""
+
+setup_gpu = """
+from numpy.fft import rfft, irfft
+import numpy as np
+n_swarm = 32768
+n_swarm39 = n_swarm * 39
+snapspersec = 2496e6 / n_swarm
+n_r2dbe39 = 2097152 / 2
+x_swarm = np.random.randn(n_swarm).astype(np.float32)
+x_swarm39 = np.random.randn(n_swarm39)
+f_swarm = rfft(x_swarm).astype(np.complex64)
+t_swarm39 = np.arange(n_swarm39) / 2496.
+t_r2dbe39 = np.arange(n_r2dbe39) / 2048.
+inn = np.round(t_r2dbe39 * 2496.).astype(int)
+ia = np.floor(t_r2dbe39 * 2496.).astype(int)
+ib = np.ceil(t_r2dbe39 * 2496.).astype(int)
+ib[(ia-ib == 0)] += 1
+tt = t_swarm39[ib] - t_swarm39[ia]
+cb = (t_r2dbe39 - t_swarm39[ia])/tt
+ca = 1.-cb
+
+import pycuda.autoinit
+import pycuda.gpuarray as gpuarray
+import scikits.cuda.fft as cu_fft
+
+plan = cu_fft.Plan(f_swarm.size,np.complex64,np.float32)
+d_f_swarm = gpuarray.to_gpu(f_swarm) 
+d_x_swarm = gpuarray.empty(x_swarm.size,dtype=np.float32) 
+"""
+
+exec(setup_cpu)
+
+test1 = """
+y = irfft(f_swarm)
+"""
+n = 1000
+t1 = timeit(test1, setup_cpu, number=n)
+t1n = t1 * snapspersec / n
+
+c_test1 = """
+cu_fft.ifft(d_f_swarm,d_x_swarm,plan)
+"""
+n = 1000
+c_t1 = timeit(c_test1, setup_gpu, number=n)
+t1n = c_t1 * snapspersec / n
+
+test2 = """
+y = fftwirfft(f_swarm)
+"""
+n = 1000
+t2 = timeit(test2, setup_cpu, number=n)
+t2n = t2 * snapspersec / n
+
+test3 = """
+y = resample(x_swarm39, n_r2dbe39)
+"""
+n = 100
+t3 = timeit(test3, setup_cpu, number=n)
+t3n = t3 * snapspersec / 39. / n
+
+test4 = """
+y = x_swarm39[inn]
+"""
+n = 10000
+t4 = timeit(test4, setup_cpu, number=n)
+t4n = t4 * snapspersec / 39. / n
+
+test5 = """
+y = ca * x_swarm39[ia] + cb * x_swarm39[ib]
+"""
+n = 100
+t5 = timeit(test5, setup_cpu, number=n)
+t5n = t5 * snapspersec / 39. / n
+
