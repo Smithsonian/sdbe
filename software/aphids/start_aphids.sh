147320ac17b3eebff19e0ffc9214f46fd3afb47e
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-06-22 07:51:01 -0700

    [start_aphids.sh] Added a parameter that allows user to define hashpipe instance ID.
    This should now be run as follows:
    
    $ ./start_aphids $HASHPIPE_INSTANCE
    
    where HASHPIPE_INSTANCE should be a number starting from 0. Each hashpipe instance will
    run in parallel with other instances.

--- software/aphids/start_aphids.sh
+++ software/aphids/start_aphids.sh
@@ -2,4 +2,4 @@
 CPU_IN=1
 CPU_OUT=2
 CPU_IN_OUT=3
-hashpipe -p aphids -c $CPU_IN vdif_in_null_thread -c $CPU_IN_OUT vdif_inout_null_thread -c $CPU_OUT vdif_out_null_thread > output.log & 
+hashpipe -I $1 -p aphids -c $CPU_IN vdif_in_null_thread -c $CPU_IN_OUT vdif_inout_null_thread -c $CPU_OUT vdif_out_null_thread > output.log & 
