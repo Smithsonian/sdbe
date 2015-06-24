ec1cbd23c47d2f9dd52135f238862e01df291f01
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-06-24 11:18:13 -0700

    [aphids_start.py] The APHIDS startup script now uses argparse to read the command line.
    Use ./aphids_start.py --help to see the list of arguments. By default just running the script
    will start all null-type threads as instance 0 with CPU affinities as 1, 2, and 3. All of these
    options however can be overwritten. In particular the thread types for the input and output threads
    may be set to "file" while will use the file-type threads from the last commit.

--- software/aphids/aphids_start.py
+++ software/aphids/aphids_start.py
@@ -2,21 +2,30 @@
 
 from subprocess import Popen
 
-CPU_IN = 1
-CPU_INOUT = 2
-CPU_OUT = 3
-
-HASHPIPE_CMD = "hashpipe -I {0} -p aphids -c {1} vdif_in_null_thread -c ${2} vdif_inout_null_thread -c {3} vdif_out_null_thread"
+HASHPIPE_CMD = "hashpipe -I {0.instance} -p aphids -c {0.cpu_in} vdif_in_{0.in_type}_thread -c ${0.cpu_inout} vdif_inout_null_thread -c {0.cpu_out} vdif_out_{0.out_type}_thread"
 
 if __name__ == "__main__":
 
-    import sys
+    from argparse import ArgumentParser
 
-    ID = int(sys.argv[1])
+    parser = ArgumentParser(description="Start an APHIDS hashpipe instance")
+
+    parser.add_argument("-i", "--instance", metavar="ID", type=int, default=0,
+                        help="instance ID to start (default=0)")
+    parser.add_argument("--cpu-in", metavar="CPU_IN", type=int, default=1,
+                        help="set CPU affinity to CPU_IN for the input thread (default=1)")
+    parser.add_argument("--cpu-inout", metavar="CPU_INOUT", type=int, default=2,
+                        help="set CPU affinity to CPU_INOUT for the input/output thread (default=2)")
+    parser.add_argument("--cpu-out", metavar="CPU_OUT", type=int, default=3,
+                        help="set CPU affinity to CPU_OUT for the output thread (default=3)")
+    parser.add_argument("--in-type", metavar="IN_TYPE", type=str, default="null", choices=["null", "file"],
+                        help="set the type of input thread (default='null')")
+    parser.add_argument("--out-type", metavar="OUT_TYPE", type=str, default="null", choices=["null", "file"],
+                        help="set the type of output thread (default='null')")
+    args = parser.parse_args()
 
     with open("stdout.log", "w") as stdout, open("stderr.log", "w") as stderr:
-        formatted_cmd = HASHPIPE_CMD.format(ID, CPU_IN, CPU_INOUT, CPU_OUT)
-        process = Popen(formatted_cmd.split(), stdout=stdout, stderr=stderr)
+        process = Popen(HASHPIPE_CMD.format(args).split(), stdout=stdout, stderr=stderr)
 
-    with open("/tmp/aphids.{0}.pid".format(ID), "w") as pidfile:
+    with open("/tmp/aphids.{0}.pid".format(args.instance), "w") as pidfile:
         pidfile.write("{0}".format(process.pid))
