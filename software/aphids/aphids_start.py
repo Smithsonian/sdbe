46f3d122efb5fdefdc9af2c70fcc31f5d54fe005
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-06-24 07:26:54 -0700

    [aphids] Replaced start_aphids.sh with equivalent aphids_start.py.
    This will allow more powerful control over how to start the APHIDS hashpipe
    instance and which threads to run, etc.

---
+++ software/aphids/aphids_start.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from subprocess import Popen
+
+CPU_IN = 1
+CPU_INOUT = 2
+CPU_OUT = 3
+
+HASHPIPE_CMD = "hashpipe -I {0} -p aphids -c {1} vdif_in_null_thread -c ${2} vdif_inout_null_thread -c {3} vdif_out_null_thread"
+
+if __name__ == "__main__":
+
+    import sys
+
+    ID = int(sys.argv[1])
+
+    with open("stdout.log", "w") as stdout, open("stderr.log", "w") as stderr:
+        formatted_cmd = HASHPIPE_CMD.format(ID, CPU_IN, CPU_INOUT, CPU_OUT)
+        process = Popen(formatted_cmd.split(), stdout=stdout, stderr=stderr)
+        print process.pid, formatted_cmd
--- software/aphids/start_aphids.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-CPU_IN=1
-CPU_OUT=2
-CPU_IN_OUT=3
-hashpipe -I $1 -p aphids -c $CPU_IN vdif_in_null_thread -c $CPU_IN_OUT vdif_inout_null_thread -c $CPU_OUT vdif_out_null_thread > output.log & 
