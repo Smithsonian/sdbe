7c06474caa12de08a243426de1eca7ea2f734e5e
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-07-07 08:43:32 -0700

    [aphids] Moved the APHIDS get/set commands to their own aphids_db module.

--- software/aphids/aphids.h
+++ software/aphids/aphids.h
@@ -11,13 +11,6 @@
 
 #define APHIDS_ERR_INIT_FAIL  -1
 #define APHIDS_ERR_CTX_UNINIT -2
-#define APHIDS_ERR_GET_FAIL   -3
-#define APHIDS_ERR_SET_FAIL   -4
-
-#define APHIDS_REDIS_HOST "127.0.0.1"
-#define APHIDS_REDIS_PORT 6379
-#define APHIDS_REDIS_TIMEOUT 1 // seconds
-
 #define APHIDS_UPDATE_EVERY 1000000
 
 /* aphids_context_t
@@ -50,23 +43,12 @@ int aphids_init(aphids_context_t * aphids_ctx, hashpipe_thread_args_t * thread_a
 */
 int aphids_update(aphids_context_t * aphids_ctx);
 
-/* aphids_set, aphids_get
-
-   These function set an APHIDS key to a certain value on a local redis 
-   server. The keys have a prefix assigned to them that depends on the thread
-   name and hashpipe instance ID, the keys follow this pattern:
-
-   aphids[<instance_id>]:<thread_name>:<key>
-
-*/
-int aphids_set(aphids_context_t * aphids_ctx, char * key, char * value);
-int aphids_get(aphids_context_t * aphids_ctx, char * key, char * value);
-
 /* aphids_destroy
 
    This function cleans up and destroys an APHIDS context and should
    at the end of all APHIDS-enable hashpipe threads.
 */
 int aphids_destroy(aphids_context_t * aphids_ctx);
+#include "aphids_db.h"
 
 #endif
