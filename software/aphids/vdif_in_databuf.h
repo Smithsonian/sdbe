a0a7f33d04d53007a595e3509b0819731fcfed1a
Andre Young <andre.young@cfa.harvard.edu>
2015-07-16 10:57:32 -0400

    Define input VDIF header structure
    Use modified version of VDIFHeader in vdif.h (m6support-0.14) to include SWARM B-engine header

--- software/aphids/vdif_in_databuf.h
+++ software/aphids/vdif_in_databuf.h
@@ -9,8 +9,39 @@
 #define VDIF_IN_PKTS_PER_BLOCK 16
 #define VDIF_IN_BUFFER_SIZE 8
 
+typedef struct vdif_in_hdr {
+    struct word0 {
+        uint32_t secs_inre:30;
+        uint32_t legacy:1;
+        uint32_t invalid:1;
+    } w0;
+    struct word1 {
+        uint32_t df_num_insec:24;
+        uint32_t ref_epoch:6;
+        uint32_t UA:2;
+    } w1;
+    struct word2 {
+        uint32_t df_len:24;
+        uint32_t num_channels:5;
+        uint32_t ver:3;
+    } w2;
+    struct word3 {
+        uint32_t stationID:16;
+        uint32_t threadID:10;
+        uint32_t bps:5;
+        uint32_t dt:1;
+    } w3;
+    struct beng_hdr {
+        uint64_t b:40;
+        uint64_t f:8;
+        uint64_t z:8;
+        uint64_t c:8;
+    } beng;
+    uint64_t edh_psn;
+} vdif_in_hdr_t;
+
 typedef struct vdif_in_packet {
-  char header[VDIF_IN_PKT_HEADER_SIZE];
+  vdif_in_hdr_t header;
   char data[VDIF_IN_PKT_DATA_SIZE];
 } vdif_in_packet_t;
 
