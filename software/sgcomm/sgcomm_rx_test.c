684aaa85315e531b68a184b20866872c176cc2b5
Andre Young <andre.young@cfa.harvard.edu>
2015-08-14 17:23:43 -0400

    Add run-time parameters to control scatter-gather output
    Changed from fixed order argument list to option-value pairs. Modules
    and disks used for writing scatter-gather files can now be set using
    command line parameters.

--- software/sgcomm/sgcomm_rx_test.c
+++ software/sgcomm/sgcomm_rx_test.c
@@ -1,8 +1,10 @@
+#include <getopt.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 #include <unistd.h>
 
@@ -33,8 +35,25 @@ sgcomm_thread st_main = {
 void handle_sigint(int signum);
 void handle_exit(void);
 
+/* Arguments */
+// default values
+char *fmtstr = "/mnt/disks/%u/%u/data/%s";
+char *pattern_write = "output.vdif";
+char *host = "localhost";
+uint16_t port = 61234;
+int n_mod = 4;
+int mod_list[4] = { 1, 2, 3, 4};
+int n_disk = 8;
+int disk_list_write[8] = { 1, 0, 2, 3, 4, 5, 6, 7 };
+// parse from command line
+void parse_arguments(int argc, char **argv);
+void print_usage(const char *cmd);
+void print_arguments(void);
+int csv_to_int(char *str, int **list); 
+
 /* Misc */
 void print_rusage(const char *tag,struct rusage *ru);
+
 
 int main(int argc, char **argv) {
 	/* Slave threads */
@@ -42,18 +61,9 @@ int main(int argc, char **argv) {
 	sgcomm_thread *st_wr; // Writer
 	shared_buffer *sbrx; // shared buffer for receive+write
 	
-	/* Reader message parameters */
-	char *fmtstr = "/mnt/disks/%u/%u/data/%s";
-	char *pattern_write = "output.vdif";
-	char *host = "localhost";
-	uint16_t port = 61234;
-	int n_mod = 4;
-	int mod_list[4] = { 1, 2, 3, 4};
-	int n_disk = 8;
-	int disk_list_read[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-	int disk_list_write[8] = { 1, 0, 2, 3, 4, 5, 6, 7 };
+	// set up according to input arguments
+	parse_arguments(argc,argv);
 	
-	/* Transmitter message parameters */
 	if (argc > 1)
 		pattern_write = argv[1];
 	if (argc > 2)
@@ -173,6 +183,153 @@ int main(int argc, char **argv) {
 	/* That's all folks! */
 	// TODO: Report that we're done
 	return EXIT_SUCCESS;
+}
+
+void parse_arguments(int argc, char **argv) {
+	int c;
+	int *tmp_list;
+	int ii;
+	
+	while (1)
+	{
+		static struct option long_options[] =
+		{
+			{"address"              , required_argument, 0, 'a'},
+			{"disk-list"            , required_argument, 0, 'd'},
+			{"output-format-string" , required_argument, 0, 'f'},
+			{"help"                 , no_argument,       0, 'h'},
+			{"module-list"          , required_argument, 0, 'm'},
+			{"output"               , required_argument, 0, 'o'},
+			{"port"                 , required_argument, 0, 'p'},
+			{0, 0, 0, 0}
+		};
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+		
+		c = getopt_long(argc, argv, "a:d:f:hm:o:p:",
+							long_options, &option_index);
+	
+		/* Detect the end of the options. */
+		if (c == -1)
+		{
+			break;
+		}
+		
+		switch (c)
+		{
+			case 'a':
+				host = optarg;
+				break;
+			case 'd':
+				n_disk = csv_to_int(optarg, &tmp_list);
+				for (ii=0; ii<n_disk; ii++) {
+					disk_list_write[ii] = tmp_list[ii];
+				}
+				free(tmp_list);
+				break;
+			case 'f':
+				fmtstr = optarg;
+				break;
+			case 'h':
+				print_usage(argv[0]);
+				exit(EXIT_SUCCESS);
+				break;
+			case 'm':
+				n_mod = csv_to_int(optarg, &tmp_list);
+				for (ii=0; ii<n_mod; ii++) {
+					mod_list[ii] = tmp_list[ii];
+				}
+				free(tmp_list);
+				break;
+			case 'o':
+				pattern_write = optarg;
+				break;
+			case 'p':
+				port = (uint16_t)atoi(optarg);
+				break;
+			case '?':
+				/* getopt_long already printed an error message. */
+				break;
+			default:
+				exit(EXIT_FAILURE);
+		}
+	}
+	print_arguments();
+}
+
+void print_usage(const char *cmd) {
+	printf(
+		"Usage: %s [OPTIONS]\n"
+		"Receive VDIF packets on network socket and write to scatter-gather.\n"
+		"\n"
+		"Mandatory arguments to long options are mandatory to short options too.\n"
+		"-a, --address=ADDR                  network address at which to receive packets\n"
+		"-d, --disk-list=LIST                comma-separated list of disks to write to\n"
+		"-f, --output-format-string=FMTSTR   format string to convert mod-/disk-list and\n"
+		"                                    output filename to full path\n"
+		"-f, --help                          display this message\n"
+		"-m, --module-list=LIST              comma-separated list of modules to write to\n"
+		"-o, --output                        output filename\n"
+		"-p, --port                          network port on which to receive packets\n"
+		"\n"
+		"The full path of output files are built using a printf-style substitution of\n"
+		"each module, each disk, and the output filename in the format string, which\n"
+		"should contain two unsigned integer (%%u) and one string (%%s) substitutions,\n"
+		"in that order. For example, given the format string '/mnt/disks/%%u/%%u/data/%%s',\n"
+		"disk-list {0,1}, module-list {3,4}, and output filename 'test.vdif', the output\n"
+		"files created are:\n"
+		"\t/mnt/disks/3/0/data/test.vdif\n"
+		"\t/mnt/disks/3/1/data/test.vdif\n"
+		"\t/mnt/disks/4/0/data/test.vdif\n"
+		"\t/mnt/disks/4/1/data/test.vdif\n"
+		"\n"
+		,cmd);
+}
+
+void print_arguments(void) {
+	char mod_list_str[256];
+	char disk_list_str[256];
+	int ii;
+	int cidx;
+	cidx = 0;
+	for (ii=0; ii<n_mod; ii++) {
+		if (ii>0) {
+			sprintf(mod_list_str+cidx,",");
+			cidx++;
+		}
+		sprintf(mod_list_str+cidx,"%2d",mod_list[ii]);
+		cidx += 2;
+	}
+	cidx = 0;
+	for (ii=0; ii<n_disk; ii++) {
+		if (ii>0) {
+			sprintf(disk_list_str+cidx,",");
+			cidx++;
+		}
+		sprintf(disk_list_str+cidx,"%2d",disk_list_write[ii]);
+		cidx += 2;
+	}
+	printf(
+		"Called with:\n"
+		"\tfmtstr=%s\n"
+		"\tpattern_write=%s\n"
+		"\thost=%s\n"
+		"\tport=%d\n"
+		"\tmod_list=%s\n"
+		"\tdisk_list=%s\n"
+		,fmtstr,pattern_write,host,(int)port,mod_list_str,disk_list_str);
+}
+
+int csv_to_int(char *str, int **list) {
+	char *num_str;
+	int ii = 0;
+	*list = (int *)malloc(100*sizeof(int));
+	num_str = strtok(str, ",");
+	while (num_str != NULL) {
+		(*list)[ii++] = atoi(num_str);
+		num_str = strtok(NULL,",");
+	}
+	return ii;
 }
 
 void handle_exit(void) {
