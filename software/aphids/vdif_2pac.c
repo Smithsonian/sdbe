2e32539337cfc1604b7fb294d903573fe2afd11c
anyoung <andre.young@cfa.harvard.edu>
2016-05-02 10:27:46 -0700

    Copy first packed VDIF header invalid data flag into second packed VDIF header

--- software/aphids/vdif_2pac.c
+++ software/aphids/vdif_2pac.c
@@ -31,12 +31,17 @@ void fix_2pac_psn(vdif_2pac_packet_t *pkt) {
 	}
 }
 
+void fix_2pac_flags(vdif_2pac_packet_t *pkt) {
+	pkt->pkt2.header.w0.invalid = pkt->pkt1.header.w0.invalid;
+}
+
 void unpack_2pac(vdif_in_packet_t *dest, vdif_2pac_packet_t *src, int n_src) {
 	int tupac_count = 0, vdif_count = 0;
 	for (tupac_count=0; tupac_count<n_src; tupac_count++) {
 		reset_2pac_size(src+tupac_count);
 		fix_2pac_data_frame(src+tupac_count);
 		fix_2pac_psn(src+tupac_count);
+		fix_2pac_flags(src+tupac_count);
 		memcpy(dest+vdif_count++,&((src+tupac_count)->pkt1),sizeof(vdif_in_packet_t));
 		memcpy(dest+vdif_count++,&((src+tupac_count)->pkt2),sizeof(vdif_in_packet_t));
 	}
